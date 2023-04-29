ceb415cf0a76a0a68362260455fa2f69c956c225
Itishree602 <122922402+Itishree602@users.noreply.github.com>
2023-04-30 02:29:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import "./styles.css";
+import AddingFlights from "./FlightsBook";
+import AddingHotels from "./HotelsBook";
+import AddingTrains from "./TrainsBook";
+import Login from "./Login";
+import Register from "./Register";
+import CheckoutPage from "./checkoutPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar"; 
 
-
+export default function App() {
   return (
-    <div id="main">
-    </div>
-  )
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<AddingFlights />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/flights" element={<AddingFlights />} />
+          <Route path="/hotels" element={<AddingHotels />} />
+          <Route path="/trains" element={<AddingTrains />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
-
-
-export default App;
